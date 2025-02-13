90e8d08dcb614cb02c72b99ff8f736434f3274f4
belanghay <113161874+belanghay@users.noreply.github.com>
2025-02-12 18:25:06 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+  
+      const titles = ('Happy Valentines Day Ming-Ming ♥').split('')
+      const titleElement = document.getElementById('title');
+      let index = 0;
+  
+      function appendTitle() {
+        if (index < titles.length) {
+          titleElement.innerHTML += titles[index];
+          index++;
+          setTimeout(appendTitle, 300); 
+        }
+      }
+  
+      appendTitle();
+  
+      clearTimeout(c);
+    }, 1000);
+  };
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
